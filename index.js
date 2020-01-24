aa93cc27b1aa5e2a98619a20a2c639f8decf7628
Daniel Schimanski <danny.manski135@gmail.com>
2020-01-24 18:56:51 +1300

    Decent amount of changes to webpage

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const apiRoot = new URL("https://swapi.co/api/");
-const searchCategories = {};
 const options = document.getElementById("options");
+const chevron = document.getElementById("chevron");
 let activeCategoryNode = document.getElementById("activeCategory");
 let activeCategory;
-let categoriesVisible = false;
+let optionsVisible = false;
+const searchCategories = {};
+let items;
 
 const setActiveCategory = (category) => {
     const lastIndex = activeCategoryNode.childNodes.length - 1
@@ -13,6 +15,7 @@ const setActiveCategory = (category) => {
         activeCategoryNode.appendChild(document.createTextNode(` ${category}`));
     }
     activeCategory = category;
+    setupCategoryMenu();
 };
 
 const populateSearchCategories = (data) => {
@@ -20,15 +23,16 @@ const populateSearchCategories = (data) => {
 };
 
 const toggleOptions = () => {
-    const chevron = document.getElementById("chevron");
-    if (options.style.display === "none") {
+    if (!optionsVisible) {
         options.style.display = "block";
         chevron.className = "fas fa-chevron-down";
+        optionsVisible = true;
     } else {
         options.style.display = "none";
         chevron.className = "fas fa-chevron-right";
+        optionsVisible = false;
     }
-}
+};
 
 const setDefaultCategory = () => {
     let defaultCategory = null;
@@ -42,11 +46,10 @@ const setDefaultCategory = () => {
     activeCategoryNode.addEventListener("click", toggleOptions);
 };
 
-const clickOption = (event) => {
-    console.log("Clicked node", event.target.textContent);
-}
-
 const setupCategoryMenu = () => {
+    while (options.lastChild) {
+        options.removeChild(options.lastChild);
+    }
     for (category in searchCategories) {
         if (category !== activeCategory) {
             let node = document.createElement("div");
@@ -56,15 +59,88 @@ const setupCategoryMenu = () => {
             options.appendChild(node);
         }
     }
-}
+};
+
+const updateLoadingBar = (count, progress) => {
+    const fullLength = document.getElementById("loadingBar").clientWidth;
+    const fraction = progress / count;
+    document.getElementById("loadingProgress").style.width = `${fraction * fullLength}px`;
+};
+
+const removeLoading = () => {
+    const loading = document.getElementById("loading");
+    loading.style.display = "none";
+};
+
+const clearContent = (parent) => {
+    while (parent.lastChild) {
+        parent.removeChild(parent.lastChild);
+    }
+};
+
+const getContentData = async (url) => {
+    let response = await fetch(url);
+    let data = await response.json();
+    let results = await data.results;
+    updateLoadingBar(data.count, results.length);
+    while (data.next) {
+        response = await fetch(data.next);
+        data = await response.json();
+        results = results.concat(data.results);
+        updateLoadingBar(data.count, results.length);
+    }
+    
+    return results;
+};
+
+const formatKey = (key) => {
+    let normalised = key.charAt(0).toUpperCase() + key.substring(1);
+    normalised = normalised.replace(/_/g, " ");
+    return normalised;
+};
+
+const formatData = (data) => {
+    console.log(typeof data);
+    return data;
+};
+
+const displayItems = (items) => {
+    removeLoading();
+    const grid = document.getElementById("grid");
+    clearContent(grid);
+    let div;
+    for (let item of items) {
+        div = document.createElement("div");
+        div.classList.add("gridItem");
+        for (let property in item) {
+            if (div.childNodes.length !== 0) {
+                div.appendChild(document.createElement("br"));
+            }
+            div.appendChild(document.createTextNode(`${formatKey(property)}: ${formatData(item[property])}`));
+        }
+        grid.appendChild(div);
+    }
+};
+
+const clickOption = (event) => {
+    setActiveCategory(event.target.textContent);
+    toggleOptions();
+    
+};
+
+/**
+ * This function is the main setup function of the page, using the helper functions defined above to achieve this.
+ * @param {URL} root 
+ */
 
 const setupPage = async (root) => {
-    const response = await fetch(root);
-    const data = await response.json()
-    populateSearchCategories(data);
+    let response = await fetch(root);
+    let data = await response.json();
+    populateSearchCategories(await data);
     setDefaultCategory();
-
-    setupCategoryMenu();
+    items = await getContentData(new URL(activeCategory, root));
+    displayItems(items);
+    console.log(items);
 };
 
 
